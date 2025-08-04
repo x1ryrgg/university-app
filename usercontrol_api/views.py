714ecd4261122bdc7c11794688e4045601126756
danilzhd <zhdamarovd@inbox.ru>
2025-08-04 11:29:05 +0300

    test

--- usercontrol_api/views.py
+++ usercontrol_api/views.py
@@ -150,3 +150,8 @@ class LinkTelegramId(APIView):
 #             serializer.save()
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TestView(ModelViewSet):
+    serializer_class = GroupSerializer
+    model = Group
